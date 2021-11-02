fd2d940e2c31100520366848ca7303fec0cf85fe
yujiewangg <ajn9598@gmail.com>
2021-11-02 16:10:34 +0800

    This is my first commit

---
+++ averge.java
@@ -0,0 +1,7 @@
+public class Average {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
+        int result = (arr[0] + arr[arr.length - 1])* arr.length / 2;
+        System.out.println(result);
+    }
+}
